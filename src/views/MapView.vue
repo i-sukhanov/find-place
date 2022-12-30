b54f4ba805dc1fe8be54bf3e0303617b61a66a79
Ivan Sukhanov <sukhanov12@yandex.ru>
2022-12-30 18:47:05 +0400

    initialization

---
+++ src/views/MapView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="map"></div>
+</template>
+
+<script lang="ts" setup></script>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+};
